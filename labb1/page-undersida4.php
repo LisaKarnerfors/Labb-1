4af56b76607aebfc8821fd34b7bc546c7631d0aa
Lisa Karnerfors <lisa.karnerfors@live.com>
2022-04-11 17:22:52 +0200

    Changed some code

--- labb1/page-undersida4.php
+++ labb1/page-undersida4.php
@@ -2,23 +2,23 @@
 <?php get_header();?>
 
 <main>
-			<section>
-				<div class="container">
-					<div class="row">
-						<div class="col-xs-12 col-sm-8 col-md-6">
-						<?php
-    						while (have_posts()) {
-        						the_post(); ?>
-            						<h1><?php the_title(); ?></h1>
-            						<p><?php the_content(); ?></p> 
-						</div>
-						<div class="col-xs-12 col-sm-4 col-md-6">
-                            <?php the_post_thumbnail(); ?>
-                        <?php } ?>
-						</div>
-					</div>
+	<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-8 col-md-6">
+				<?php
+					while (have_posts()) {
+						the_post(); ?>
+							<h1><?php the_title(); ?></h1>
+							<p><?php the_content(); ?></p> 
 				</div>
-			</section>
-		</main>
+				<div class="col-xs-12 col-sm-4 col-md-6">
+					<?php the_post_thumbnail(); ?>
+				<?php } ?>
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
 
 <?php get_footer();?>
